e3fc7ee7a42867f3e359d85867e9af99dc267ba9
Klipspringer <klipspringr@users.noreply.github.com>
2025-03-28 14:41:57 +0000

    don't use GM.getValue, Chrome security appears to reject it

--- mefi-replace-quote-label.user.js
+++ mefi-replace-quote-label.user.js
@@ -1,17 +1,25 @@
 // ==UserScript==
 // @name         MeFi replace quote label
 // @namespace    https://github.com/klipspringr/mefi-scripts
-// @version      2025-03-28
+// @version      2025-03-28-c
 // @description  Replace the label on the quote button
 // @author       Klipspringer
 // @match        *://*.metafilter.com/*
-// @grant        GM.getValue
-// // ==/UserScript==
+// @grant        none
+// ==/UserScript==
+
+const getSetting = (key, defaultValue) => {
+  try {
+    return localStorage.getItem(key) || defaultValue;
+  } catch {
+    return defaultValue;
+  }
+};
 
 (async () => {
   if (!/^\/(\d|comments\.mefi)/.test(window.location.pathname)) return;
 
-  const to = (await GM.getValue()) || "↩ ";
+  const to = getSetting("mefi-replace-quote-label", "↩ ");
   document
     .querySelectorAll('a[class="quotebutton"]')
     .forEach((node) => (node.textContent = to));
