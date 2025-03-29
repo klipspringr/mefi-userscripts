3b682d0eee27037ec3c104adc4f7da605d2b1b3c
Klipspringer <klipspringr@users.noreply.github.com>
2025-03-29 14:10:18 +0000

    move quote button to between favourites and flag

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 
 MetaFilter's [built-in MefiQuote functionality](https://metatalk.metafilter.com/26605/Better-MetaFiltering-through-scripting) adds a quote button to each comment.
 
-This script replaces the "quote" label with an arrow symbol, for a shorter and more consistent comment byline.
+This script neatens up the comment byline by replacing "quote" with a reply arrow, and moving it to between + and ⚑.
 
-Or, for a custom label, use your browser's developer tools to set the Local Storage key `mefi-replace-quote-label` to the desired string.
+For a custom label, use your browser's developer tools to set the Local Storage key `mefi-replace-quote-label` to the desired string.
 
 ![MeFi replace quote label example](assets/mefi-replace-quote-label.png)
--- mefi-replace-quote-label.user.js
+++ mefi-replace-quote-label.user.js
@@ -2,7 +2,7 @@
 // @name         MeFi replace quote label
 // @namespace    https://github.com/klipspringr/mefi-userscripts
 // @version      2025-03-28-h
-// @description  MetaFilter: replace the MefiQuote label with an arrow, or a custom label
+// @description  MetaFilter: nicer MefiQuote buttons
 // @author       Klipspringer
 // @supportURL   https://github.com/klipspringr/mefi-userscripts
 // @license      MIT
@@ -11,34 +11,63 @@
 // ==/UserScript==
 
 const getSetting = (key, defaultValue) => {
-  try {
-    return localStorage.getItem(key) || defaultValue;
-  } catch {
-    return defaultValue;
-  }
+    try {
+        return localStorage.getItem(key) || defaultValue;
+    } catch {
+        return defaultValue;
+    }
+};
+
+const modifyQuoteButton = (quoteButtonNode, to) => {
+    // replace quote button content
+    quoteButtonNode.textContent = to;
+
+    // move quote button to before flag button
+    const quoteNode = quoteButtonNode.parentNode;
+    const bylineNode = quoteNode.parentNode;
+    const flagNode = bylineNode.querySelector("span[id^='flag']");
+    if (flagNode) bylineNode.insertBefore(quoteNode, flagNode);
+
+    // mark as done so we don't pick this up again
+    quoteButtonNode.setAttribute("data-mrql-done", "");
+};
+
+const modifyQuoteButtons = (to) => {
+    const start = performance.now();
+
+    const quoteButtonNodes = document.querySelectorAll(
+        'a[class="quotebutton"]:not([data-mrql-done])'
+    );
+
+    for (const quoteButtonNode of quoteButtonNodes)
+        modifyQuoteButton(quoteButtonNode, to);
+
+    console.debug(
+        "mefi-replace-quote-label",
+        quoteButtonNodes.length,
+        `"${to}"`,
+        performance.now() - start + "ms"
+    );
 };
 
 (() => {
-  if (!/^\/(\d|comments\.mefi)/.test(window.location.pathname)) return;
+    if (!/^\/(\d|comments\.mefi)/.test(window.location.pathname)) return;
 
-  const to = getSetting("mefi-replace-quote-label", "↩ "); // note space, for aesthetics
+    const DEFAULT_TO = "↩ "; // note space, for aesthetics
 
-  const replaceQuoteLabels = () => {
-    const nodes = document.querySelectorAll('a[class="quotebutton"]');
-    nodes.forEach((node) => (node.textContent = to));
-    console.log(`mefi-replace-quote-label: ${nodes.length} labels to "${to}"`);
-  };
+    const to = getSetting("mefi-replace-quote-label", DEFAULT_TO);
 
-  const newCommentsWrapper = document.getElementById("newcomments");
-  if (newCommentsWrapper) {
+    const runner = () => modifyQuoteButtons(to);
+
     // MefiQuote listens for the "mefi-comments" event, but:
     //    (a) my event listener wasn't picking that up, for some reason; and
-    //    (b) there would be timing issues as MefiQuote needs to complete its work first
-    // hence using MutationObserver instead
-    const observer = new MutationObserver(() => replaceQuoteLabels());
-    // listen for "childList" mutations, but not subtree as *we* are mutating that
-    observer.observe(newCommentsWrapper, { childList: true });
-  }
+    //    (b) there could be timing issues as MefiQuote needs to complete its work first
+    // hence using MutationObserver instead.
+    const newCommentsElement = document.getElementById("newcomments");
+    if (newCommentsElement) {
+        const observer = new MutationObserver(runner);
+        observer.observe(newCommentsElement, { childList: true });
+    }
 
-  replaceQuoteLabels();
+    runner();
 })();
