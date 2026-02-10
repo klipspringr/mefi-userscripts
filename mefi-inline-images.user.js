8b0a3725670797c571a8e8ffe43b2293c03d2fd6
Klipspringer <klipspringr@users.noreply.github.com>
2026-02-10 20:04:03 +0000

    mefi-inline-images tweaks, update dependencies

--- mefi-inline-images.user.js
+++ mefi-inline-images.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MeFi Inline Images
 // @namespace    https://github.com/klipspringr/mefi-userscripts
-// @version      2025-09-09-a
+// @version      2026-02-10-a
 // @description  MetaFilter: restore the glory days of the IMG tag by inlining images linked in comments
 // @author       Klipspringer
 // @supportURL   https://github.com/klipspringr/mefi-userscripts
@@ -17,6 +17,9 @@
     const IMG_CLASS = "mfit"
     const ATTRIBUTE_DONE = "data-mfit"
 
+    const REGEX_IMAGE_PATHS = /\.(apng|avif|bmp|gif|jpg|jpeg|png|svg|webp)$/i
+    const REGEX_IMGUR_PATH = /^\/\w+\/?$/
+
     if (
         !/^\/(\d|comments\.mefi)/.test(window.location.pathname) ||
         /rss$/.test(window.location.pathname)
@@ -24,58 +27,74 @@
         return
     }
 
-    const onImageError = (e) => e.target.remove() // if fail to load, remove element
+    // if failed to load, remove element (avoids broken image icon)
+    const onImageError = (e) => e.target?.remove()
 
     const run = async (target) => {
         const start = performance.now()
 
+        let inserted = 0
+
         const nodes = (target ?? document).querySelectorAll(
-            `div.comments a:not(.smallcopy *):not([${ATTRIBUTE_DONE}])`
+            `div.comments a:not(.smallcopy *):not([${ATTRIBUTE_DONE}])`,
         )
 
-        let count = 0
+        for (const node of nodes) {
+            const href = node.getAttribute("href")
+            if (!href) {
+                continue
+            }
 
-        nodes.forEach((node) => {
-            let href = node.getAttribute("href")
-            if (!href) return
+            let url
+            try {
+                url = new URL(href)
+            } catch {
+                continue
+            }
 
-            const imgur = href.match(
-                /^https?:\/\/(?:i\.)?imgur.com\/(\w+)$/
-            )?.[1]
-            if (imgur) {
-                // using i subdomain saves a 302 redirect
-                // .png works even if MIME type is different
-                href = `https://i.imgur.com/${imgur}.png`
-            } else if (!/\.(avif|gif|jpg|jpeg|png|webp)$/i.test(href)) {
-                return
+            let imageHref
+
+            if (REGEX_IMAGE_PATHS.test(url.pathname)) {
+                imageHref = href
+            } else if (
+                url.hostname.endsWith("imgur.com") &&
+                REGEX_IMGUR_PATH.test(url.pathname)
+            ) {
+                // special case: https://imgur.com/cvxGSlG can be treated as https://i.imgur.com/cvxGSlG.png
+                // using the i subdomain saves a 302 redirect, and .png extension works for all MIME types
+                imageHref = `https://i.imgur.com${url.pathname}.png`
+            } else {
+                continue
             }
 
             // don't inject images twice - e.g. when new comments loaded a second time
             node.setAttribute(ATTRIBUTE_DONE, "done")
 
             const img = document.createElement("img")
-            img.setAttribute("src", href)
+            img.setAttribute("src", imageHref)
             img.setAttribute("class", IMG_CLASS)
             img.addEventListener("error", onImageError)
 
             // we're setting display:block on the img. so a following <br> causes an empty line
-            if (node.nextElementSibling?.tagName === "BR")
+            if (node.nextElementSibling?.tagName === "BR") {
                 node.nextElementSibling.remove()
+            }
 
             node.insertAdjacentElement("afterend", img)
 
-            count += 1
-        })
+            inserted += 1
+        }
 
         console.log(
             "mefi-inline-images",
             nodes.length,
-            count,
-            Math.round(performance.now() - start) + "ms"
+            inserted,
+            Math.round(performance.now() - start) + "ms",
         )
     }
 
     const styleElement = document.createElement("style")
+
     styleElement.textContent = `img.${IMG_CLASS} {
         display: block;
         max-width: 100%;
@@ -85,9 +104,11 @@
         object-fit: contain;
         border: 1px solid rgba(0, 0, 0, 0.2);
     }`
+
     document.body.insertAdjacentElement("beforeend", styleElement)
 
     const newCommentsElement = document.getElementById("newcomments")
+
     if (newCommentsElement) {
         const observer = new MutationObserver(() => run(newCommentsElement))
         observer.observe(newCommentsElement, { childList: true })
